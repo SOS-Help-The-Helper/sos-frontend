41877b600123a3f9caadc83fd3acdc4e839ed6e2
Henry (AI) <info@sos-help.org>
2026-05-10 23:40:03 +0000

    debug: try /portal/erv instead of /p/erv

---
+++ app/portal/erv/page.tsx
@@ -0,0 +1,3 @@
+export default function TestPage() {
+  return <div style={{color:'white',background:'#0F1E2B',height:'100vh',display:'flex',alignItems:'center',justifyContent:'center'}}>Portal Test</div>;
+}
