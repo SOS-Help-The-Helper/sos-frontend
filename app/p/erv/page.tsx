6696d271f8f5c5b3fb3e1d0d76742336002a3b70
Henry (AI) <info@sos-help.org>
2026-05-10 23:39:13 +0000

    debug: add static /p/erv page to test if /p/ path works at all

---
+++ app/p/erv/page.tsx
@@ -0,0 +1,3 @@
+export default function ERVPartnerPage() {
+  return <div style={{color: 'white', background: '#0F1E2B', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>ERV Partner Portal - Static Test</div>;
+}
