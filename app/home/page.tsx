5428630592ae85701d795854fdb61c4fc344fdb1
Jarren San Jose <sanjose.jarren@gmail.com>
2026-05-16 17:13:43 -0700

    chore: delete orphan (partner)/(gov)/(admin)/(ems) route groups + notifications.ts (#40)

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -859,7 +859,7 @@ export default function HomePage() {
           </Link>
         </p>
         <p>
-          <Link href="/c">Citizens</Link>&nbsp;&nbsp;·&nbsp;&nbsp;<Link href="/matching">Partners</Link>&nbsp;&nbsp;·&nbsp;&nbsp;<a href="mailto:info@sos-help.org">Contact</a>
+          <Link href="/c">Citizens</Link>&nbsp;&nbsp;·&nbsp;&nbsp;<Link href="/app">Partners</Link>&nbsp;&nbsp;·&nbsp;&nbsp;<a href="mailto:info@sos-help.org">Contact</a>
         </p>
         <p className="footer-copy">
           sosconnect.org &middot; © 2026 SOS Global
